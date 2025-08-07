5920d1ebb33eaec99c21ad9eab44eba8d869ffbd
KrittidejDev <krittidej.dev@gmail.com>
2025-08-07 10:15:31 +0700

    add answer mission 3

--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -6,20 +6,40 @@ function ProductHighlight() {
       <div className="container mx-auto">
         <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-          {/* 🛠️ Todo 2: ให้ส่งข้อมูลเข้าไปใน Component ProductCard ผ่าน Prop ที่ชื่อว่า `imgSrc`
-            
-            ==== ตัวอย่าง ====
-
-            <ProductCard imgSrc="https://picsum.photos/id/231/200/200" />
-            
-        */}
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {CARD_DATA &&
+            CARD_DATA.map((e) => (
+              <ProductCard
+                key={e.id}
+                imgSrc={e.imgSrc}
+                productName={e.productName}
+                productPrice={e.productPrice}
+              />
+            ))}
         </div>
       </div>
     </section>
   );
 }
+
+const CARD_DATA = [
+  {
+    id: 1,
+    imgSrc: "https://picsum.photos/id/231/200/200",
+    productName: "The Lorem Ipsum for photos.",
+    productPrice: 99.99,
+  },
+  {
+    id: 1,
+    imgSrc: "https://picsum.photos/id/232/200/200",
+    productName: "Wireless Bluetooth Earbuds",
+    productPrice: 79.99,
+  },
+  {
+    id: 1,
+    imgSrc: "https://picsum.photos/200/200",
+    productName: "Stylish Cool Sunglasses in The World",
+    productPrice: 79.99,
+  },
+];
 
 export default ProductHighlight;
